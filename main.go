e39c1c57fb827e3a0321de785ef5062ce5e00f20
Illia Lukashuk <ilu101207@gmail.com>
2020-06-27 08:45:03 +0300

    Guessing number that use guessed once

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Guess a number 1 - 100. Are you ready?")
-	
+	for {
+		fmt.Println("Guess a number 1 - 100. Are you ready?")
+		numberGuessed, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if strings.TrimSpace(numberGuessed) == "yes" {
+			break
+		}
+	}
+	min := 0
+	max := 101
+	guessedNumber := min + (max-min)/2
+	fmt.Println("Is number", guessedNumber, "<,>,= number you guessed?")
+	comparisonAnswer, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if strings.TrimSpace(comparisonAnswer) == ">" {
+		min = guessedNumber
+	}
+	if strings.TrimSpace(comparisonAnswer) == "<" {
+		max = guessedNumber
+	}
+	if strings.TrimSpace(comparisonAnswer) == "=" {
+		fmt.Println("Well done")
+	}
 }
